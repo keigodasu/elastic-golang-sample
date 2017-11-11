02b3eb8a53514617da698bfdf5c45811445740ac
keigodasu <keigodasu0524@yahoo.co.jp>
2017-11-11 17:10:24 +0900

    add QueryStringQuery sample

--- querystringquery_sample.go
+++ querystringquery_sample.go
@@ -16,16 +16,16 @@ func main() {
 	runner := QueryRunner{client: client, ctx: ctx}
 
 	// xx AND xx
-	runner.RunQuery("Urgency: 1 AND CreatedAt:2017-11-06")
+	runner.RunQuery("priority: 1 AND CreatedAt:2017-11-06")
 
 	// xx OR xx
-	runner.RunQuery("Urgency: 1 OR CreatedAt:2017-11-06")
+	runner.RunQuery("priority: 1 OR CreatedAt:2017-11-06")
 
 	// NOT xx
-	runner.RunQuery("Urgency: 1 OR NOT CreatedAt:2017-11-06")
+	runner.RunQuery("priority: 1 OR NOT CreatedAt:2017-11-06")
 
 	// (xx AND xx) OR (xx AND xx)
-	runner.RunQuery("(Urgency:1 AND CreatedAt:2017-11-06) OR (CreatedAt:2017-11-03)")
+	runner.RunQuery("(priority:1 AND CreatedAt:2017-11-06) OR (CreatedAt:2017-11-03)")
 
 	// range
 	runner.RunQuery("CreatedAt: [2017-11-01 TO 2017-11-04]")
@@ -33,9 +33,9 @@ func main() {
 	runner.RunQuery("CreatedAt:>2017-11-04")
 
 	// wildcards
-	runner.RunQuery("Displayheadline:moga*")
-	runner.RunQuery("Displayheadline:ugau?a")
+	runner.RunQuery("comment:moga*")
+	runner.RunQuery("comment:ugau?a")
 
 	//regular
-	runner.RunQuery("Displayheadline:/hogeho[gu]e/")
+	runner.RunQuery("comment:/hogeho[gu]e/")
 }
